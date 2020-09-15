1767890e10253fdaf606a41b1e140ac6eed91450
Sarah Busert <sarah.busert@ipm.fraunhofer.de>
2020-09-15 09:41:39 +0200

    Simple: testing SetRenderPause and CloseGameWindow

--- Examples/Complete/Simple/Desktop/Main.cs
+++ Examples/Complete/Simple/Desktop/Main.cs
@@ -1,10 +1,36 @@
-﻿namespace Fusee.Examples.Simple.Desktop
+﻿using Fusee.Base.Core;
+using System;
+using System.Threading;
+
+namespace Fusee.Examples.Simple.Desktop
 {
     public class Simple
     {
         public static void Main()
         {
             Lib.ExecFusAppInNewThread();
+
+            var isInit = Lib.IsAppInitialized();
+
+            while(!isInit)
+                isInit = Lib.IsAppInitialized();
+
+            Thread.Sleep(2000);
+
+            Lib.SetRenderPause(true);
+
+            Thread.Sleep(2000);
+
+            Lib.SetRenderPause(false);
+
+            Thread.Sleep(2000);
+
+            Lib.CloseGameWindow();
+
+            var test = Lib.FusTask.IsCompleted;
+
+            Console.ReadKey();
+
         }
     }
 }
